496d82185c3460e65ea4fd32e87989e8450129fc
Chetan Surpur <chetan51@gmail.com>
2011-06-30 22:27:49 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mockies::Application.config.secret_token = '6a9c39090c04b2b17b6234cb07ea7d1a87b7c0e6cf4af8498b01a2e7c702955062ddc25be1c3d16742b87260cfc056ec559c91c3f22d435a37132086c416ad3a'
